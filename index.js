9f66c5316c65a6b3b4f1aed302302a21ae632c5f
lorenzo-jungers <142529647+lorenzo-jungers@users.noreply.github.com>
2023-08-20 08:51:01 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+let firstName = "Lorenzo";
+let age = 12;
+let student = true;
+console.log("Tudo bem ",firstName, "?");
+console.log("Você tem",age,"anos.");
+console.log("Você é estudante?",student);
+
+document.getElementById("1p").innerHTML = "Ola " + firstName;
+
+document.getElementById("2p").innerHTML = "Você tem " + age +" anos.";
+
+document.getElementById("3p").innerHTML = "Você é estudante? " + student;
+
+let x;
+x = 3 + 12 - (5*3 - 10 )
+console.log(x)
+*/
+let nome;
+ document.getElementById("Mandar").onclick = function(){
+    nome = document.getElementById("nome").value
+    console.log("Seu nome é",nome)
+    
+ }
